20ecef0f57ad1d3a52e53f8d1c503a4844935727
mich.kam97@gmail.com <mich.kam97@gmail.com>
2023-09-12 00:24:50 +0200

    fixed namespaces

--- MarketOrderSystem/Marketplace.cs
+++ MarketOrderSystem/Marketplace.cs
@@ -1,12 +1,10 @@
-﻿using Kaos.Collections;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MarketplaceOrderSystem
+namespace MarketOrderSystem
 {
     public class Marketplace
     {
--- MarketOrderSystem/Program.cs
+++ MarketOrderSystem/Program.cs
@@ -1,11 +1,10 @@
-﻿using MarketOrderSystem;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MarketplaceOrderSystem
+namespace MarketOrderSystem
 {
     class Program
     {
