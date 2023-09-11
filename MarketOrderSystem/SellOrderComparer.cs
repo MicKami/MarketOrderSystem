1f1bb28ddb706d8fffb09d2c5bfa7321a771b8ce
mich.kam97@gmail.com <mich.kam97@gmail.com>
2023-09-12 00:32:25 +0200

    Added SellOrderComparer

---
+++ MarketOrderSystem/SellOrderComparer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketOrderSystem
+{
+    internal class SellOrderComparer : IComparer<MarketOrder>
+    {
+        public int Compare(MarketOrder? x, MarketOrder? y)
+        {
+            if (x == null || y == null) return 0;
+            if (x.Price.CompareTo(y.Price) == 0)
+            {
+                return x.Timestamp.CompareTo(y.Timestamp);
+            }
+            else
+            {
+                return x.Price.CompareTo(y.Price);
+            }
+        }
+    }
+}
