d5453ef280cd466dc1aea985a1946450493c92c3
mich.kam97@gmail.com <mich.kam97@gmail.com>
2023-09-14 02:44:02 +0200

    Changed main program

--- MarketOrderSystem/Program.cs
+++ MarketOrderSystem/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using MarketplaceOrderSystem;
@@ -11,56 +12,65 @@ class Program
     {
         Marketplace market = new Marketplace();
 
-        MarketPlayer player = new MarketPlayer("Player1");
-        player.AddMoney(10000);
-        player.AddToInventory("wood", 200);
-        player.AddToInventory("stone", 300);
-        player.AddToInventory("leather", 80);
-        player.AddToInventory("iron", 50);
-        MarketOrder order1 = new()
-        {
-            ItemName = "wood",
-            OrderType = MarketOrderType.Sell,
-            Price = 26,
-            Quantity = 100
-        };
-        MarketOrder order2 = new()
-        {
-            ItemName = "stone",
-            OrderType = MarketOrderType.Buy,
-            Price = 20,
-            Quantity = 50
-        };
-        player.PlaceOrder(order1, market);
-        player.PlaceOrder(order2, market);
 
-        DisplayPlayerInventory(player);
-        DisplayOrders("wood", MarketOrderType.Sell, market);
-        DisplayOrders("stone", MarketOrderType.Buy, market);
+        MarketPlayer player1 = new MarketPlayer("Player1");
+        player1.AddToInventory("wood", 100);
+
+
+
+        MarketPlayer player2 = new MarketPlayer("Player2");
+        player2.AddMoney(10000);
+
+        player2.PlaceOrder(new MarketOrder { ItemName = "wood", OrderType = MarketOrderType.Buy, Price = 25, Quantity = 100 }, market);
+        player1.PlaceOrder(new MarketOrder { ItemName = "wood", OrderType = MarketOrderType.Sell, Price = 20, Quantity = 100 }, market);
+
+        DisplayPlayerInventory(player1);
+        DisplayPlayerOrders(player1);
+        DisplayPlayerInventory(player2);
+        DisplayPlayerOrders(player2);
+        DisplayMarketOrders("wood", market);
 
         Console.Read();
 
         static void DisplayPlayerInventory(MarketPlayer player)
         {
-            //Console.WriteLine("-------------------------------");
-            Console.WriteLine($"{player.Name}: ");
+            Console.WriteLine($"---------{player.Name} Inventory --------- ");
             Console.WriteLine($"Money: {player.GetMoney()}");
             foreach (var inventoryItem in player.GetInventory())
             {
                 Console.WriteLine($"{inventoryItem.itemName}: {inventoryItem.quantity}");
             }
-            Console.WriteLine("-------------------------------");
+            Console.WriteLine("-------------------------------\n");
+        }
+
+        static void DisplayPlayerOrders(MarketPlayer player)
+        {
+            Console.WriteLine($"---------{player.Name} Orders ---------");
+            int i = 0;
+            foreach (var order in player.GetOrders())
+            {
+                Console.WriteLine($"{i++}. {{ {order.OrderType}| {order.ItemName} | {order.Quantity} | {order.Price} }}");
+            }
+            Console.WriteLine("-------------------------------\n");
+        }
+
+        static void DisplayMarketOrders(string item, Marketplace market)
+        {
+            Console.WriteLine($"--------- {item} Market --------- ");
+            Console.WriteLine($"Buy");
+            foreach (var buyOrder in market.GetOrders(item, MarketOrderType.Buy).Reverse<MarketOrder>())
+            {
+                Console.WriteLine($"| {buyOrder.ItemName}| {buyOrder.Quantity} | {buyOrder.Price} |");
+            }
+            Console.WriteLine("+---------------+");
+            foreach (var sellOrder in market.GetOrders(item, MarketOrderType.Sell))
+            {
+                Console.WriteLine($"| {sellOrder.ItemName}| {sellOrder.Quantity} | {sellOrder.Price} |");
+            }
+            Console.WriteLine($"Sell");
+            Console.WriteLine("-------------------------------\n");
         }
     }
 
-    private static void DisplayOrders(string item, MarketOrderType orderType, Marketplace market)
-    {
-        Console.WriteLine($"{orderType}\n");
-        foreach (var buyOrder in market.GetOrders(item, orderType))
-        {
-            Console.WriteLine($"| {buyOrder.ItemName}| {buyOrder.Quantity} | {buyOrder.Price} |");
-        }
-        Console.WriteLine("\n-------------------------------");
-    }
 }
 
